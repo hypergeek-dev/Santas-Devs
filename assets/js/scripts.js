22267e2e1e7e2dbe8d05f963c2e36fb50e19f3cb
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-16 08:16:29 +0100

    snow and fire animation on landing page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,54 +1,41 @@
-// Function to update the countdown every second
+// Countdown Function
 function updateCountdown() {
-    // Set the target date for the countdown (Christmas Day)
-    const targetDate = new Date("Dec 25, 2023 00:00:00").getTime();
-    // Get the current date and time
-    const now = new Date().getTime();
-    // Calculate the difference between now and the target date
-    const difference = targetDate - now;
-  
-    // Convert the difference into days, hours, minutes, and seconds
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  
-    // Update the countdown HTML with the new time
-    document.getElementById('days').textContent = days < 10 ? '0' + days : days;
-    document.getElementById('hours').textContent = hours < 10 ? '0' + hours : hours;
-    document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
-    document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
-  
-    // If the countdown is over, clear the interval and show a message
-    if (difference < 0) {
+  const targetDate = new Date("Dec 25, 2023 00:00:00").getTime();
+  const now = new Date().getTime();
+  const difference = targetDate - now;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  document.getElementById('days').textContent = days < 10 ? '0' + days : days;
+  document.getElementById('hours').textContent = hours < 10 ? '0' + hours : hours;
+  document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
+  document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
+
+  if (difference < 0) {
       clearInterval(interval);
       document.getElementById('countdown').textContent = "Merry Christmas!";
-    }
   }
-  
-  // Initialize the countdown to update every second (1000 milliseconds)
-  const interval = setInterval(updateCountdown, 1000);
+}
 
-
-// Manually updated list of songs
-var songs = ["deck-the-halls-xmas.mp3", "carol-of-the-bells-xmas-hip-hop-music.mp3", "enchanted-chimes.mp3", "jingle-family", "joyful-jingle", "silent-night_medium", "we-wish-you-a-merry-christmas-xmas"]; // Add more songs as needed
-
-// Select elements
+// Music Playlist Functions
+var songs = ["deck-the-halls-xmas.mp3", "carol-of-the-bells-xmas-hip-hop-music.mp3", "enchanted-chimes.mp3", "jingle-family", "joyful-jingle", "silent-night_medium", "we-wish-you-a-merry-christmas-xmas"];
 var audioPlayer = document.getElementById('audioPlayer');
 var audioSource = document.getElementById('audioSource');
 var playlist = document.getElementById('playlist');
+var currentSong = 0;
 
-// Load playlist
 function loadPlaylist() {
   songs.forEach(function(song, index) {
-    var listItem = document.createElement('li');
-    listItem.textContent = song;
-    listItem.setAttribute('data-index', index);
-    playlist.appendChild(listItem);
+      var listItem = document.createElement('li');
+      listItem.textContent = song;
+      listItem.setAttribute('data-index', index);
+      playlist.appendChild(listItem);
   });
 }
 
-// Play a song
 function playSong(index) {
   currentSong = index;
   audioSource.src = `assets/music/${songs[index]}`;
@@ -56,34 +43,115 @@ function playSong(index) {
   audioPlayer.play();
 }
 
-// Attach event listeners for playlist selection
 playlist.addEventListener('click', function(e) {
   if (e.target && e.target.nodeName === "LI") {
-    playSong(e.target.getAttribute('data-index'));
+      playSong(e.target.getAttribute('data-index'));
   }
 });
 
-// Global current song index
-var currentSong = 0;
-
-// Play the next song in the playlist
 function nextSong() {
   if (currentSong < songs.length - 1) {
-    playSong(++currentSong);
+      playSong(++currentSong);
   }
 }
 
-// Play the previous song in the playlist
 function prevSong() {
   if (currentSong > 0) {
-    playSong(--currentSong);
+      playSong(--currentSong);
   }
 }
 
-// Event listeners for next and previous buttons
 document.getElementById('nextButton').addEventListener('click', nextSong);
 document.getElementById('prevButton').addEventListener('click', prevSong);
 
-
-// Initialize playlist
-loadPlaylist();
+// Snow Effect
+ // Amount of Snowflakes
+ var snowMax = 750;
+      
+ // Snowflake Colours
+ var snowColor = ["#DDD", "#EEE"];
+ 
+ // Snow Entity
+ var snowEntity = "&#x2022;";
+ 
+ // Falling Velocity
+ var snowSpeed = 0.75;
+ 
+ // Minimum Flake Size
+ var snowMinSize = 8;
+ 
+ // Maximum Flake Size
+ var snowMaxSize = 24;
+ 
+ // Refresh Rate (in milliseconds)
+ var snowRefresh = 50;
+ 
+ // Additional Styles
+ var snowStyles = "cursor: default; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;";
+ 
+ // Variables for snowflakes and position
+ var snow = [], pos = [], coords = [], lefr = [], marginBottom, marginRight, snowContainer;
+ 
+ function randomise(range) {
+     return Math.floor(range * Math.random());
+ }
+ 
+ function initSnow() {
+     snowContainer = document.getElementById('snow-container'); 
+     var snowSize = snowMaxSize - snowMinSize;
+     marginBottom = snowContainer.clientHeight - 5;
+     marginRight = snowContainer.clientWidth - 15;
+ 
+     for (var i = 0; i <= snowMax; i++) {
+         coords[i] = 0;
+         lefr[i] = Math.random() * 15;
+         pos[i] = 0.03 + Math.random() / 10;
+ 
+         var flake = document.createElement('span');
+         flake.id = 'flake' + i;
+         flake.innerHTML = snowEntity;
+         flake.style.cssText = snowStyles + "position:absolute;top:-" + snowMaxSize;
+         snowContainer.appendChild(flake);
+ 
+         snow[i] = flake;
+         snow[i].style.fontFamily = "inherit";
+         snow[i].size = randomise(snowSize) + snowMinSize;
+         snow[i].style.fontSize = snow[i].size + "px";
+         snow[i].style.color = snowColor[randomise(snowColor.length)];
+         snow[i].style.zIndex = 2;
+         snow[i].sink = snowSpeed * snow[i].size / 5;
+         snow[i].posX = randomise(marginRight - snow[i].size);
+         snow[i].posY = randomise(2 * marginBottom - marginBottom - 2 * snow[i].size);
+         snow[i].style.left = snow[i].posX + "px";
+         snow[i].style.top = snow[i].posY + "px";
+     }
+ 
+     moveSnow();
+ }
+ 
+ function resize() {
+     if (!snowContainer) {
+         return;
+     }
+     marginBottom = snowContainer.clientHeight - 5;
+     marginRight = snowContainer.clientWidth - 50;
+ }
+ 
+ function moveSnow() {
+     for (var i = 0; i <= snowMax; i++) {
+         coords[i] += pos[i];
+         snow[i].posY += snow[i].sink;
+         snow[i].style.left = snow[i].posX + lefr[i] * Math.sin(coords[i]) + "px";
+         snow[i].style.top = snow[i].posY + "px";
+ 
+         if (snow[i].posY >= marginBottom - 2 * snow[i].size || parseInt(snow[i].style.left) > (marginRight - 3 * lefr[i])) {
+             snow[i].posX = randomise(marginRight - snow[i].size);
+             snow[i].posY = 0;
+         }
+     }
+     setTimeout(moveSnow, snowRefresh);
+ }
+ 
+ // Event listeners
+ window.addEventListener('resize', resize);
+ window.addEventListener('load', initSnow);
